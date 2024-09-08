e5bdb11b05146ee382ed2ee6025e5fcb090de39f
Dinan <dinanvanw@gmail.com>
2024-09-08 15:24:16 +0200

    Update to vectors demo

--- vectors/Assets/VectorTest.cs
+++ vectors/Assets/VectorTest.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
+//using System.Numerics;
 
 //using System.Numerics;
 using UnityEngine;
@@ -28,7 +28,23 @@ public class VectorTest : MonoBehaviour
 
         double pythagoras = difX = Math.Pow(difX,2)+Math.Pow(difY,2) + Math.Pow(difZ,2);
         double distance = Math.Sqrt(pythagoras);
-        print(distance);
+        print($"Distance between cubes: {distance}");
+
+        // Dot product. Gives information about the orientation about two objects.
+        Vector3 forwardA = transform.forward;
+        Vector3 forwardB = otherCubeObject.transform.forward;
+        float dotProduct = Vector3.Dot(forwardA.normalized, forwardB.normalized);
+
+        print($"Dot Product: {dotProduct}");
+        
+        if (dotProduct >= 0.5f && dotProduct <= 1f)
+        {
+            print("Objects (almost) are almost looking into the same direction.");
+        }
+        else
+        {
+            print("Objects are not looking into the same direction");
+        }
     }
 
 
